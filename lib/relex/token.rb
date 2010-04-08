a99e1ff77fcc22a6d1bb1415feca0f8705a54a6f
Vítor Baptista <vitor@lavid.ufpb.br>
2010-04-08 13:43:31 -0300

    Remove regexps de Token. Elas devem estar em cli.rb

--- lib/relex/token.rb
+++ lib/relex/token.rb
@@ -1,16 +1,6 @@
 module Relex
   class Token
-    attr_reader :palavras_reservadas, :delimitadores, :operadores_aritmeticos, :identificadores, :strings_numericas, :comando_atribuicao, :operador_relacional
-
     def initialize(valor, classificacao)
-      self.palavras_reservadas = /program|var|begin|end|integer|if|then|else/
-      self.delimitadores = /;|:|,|\(|\)/
-      self.operadores_aritmeticos = /\+|\*/
-      self.identificadores = /[a..z]+[a..z0..9_]*/
-      self.strings_numericas = /[0..9]+/
-      self.comando_atribuicao = /\:=/
-      self.operador_relacional = /=/
-
       @valor = valor
       @classificacao = classificacao
     end
