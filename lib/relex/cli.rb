ba1f9ab3a1903204ba666c3e70f1111d3851f8f3
Vítor Baptista <vitor@lavid.ufpb.br>
2010-04-08 13:44:07 -0300

    Funciona (mais ou menos) com regexps, pegando palavra a palavra
    Tem diversos casos onde isso não dá certo. Talvez seja melhor
    seguir o autômato, pegando caracter a caracter, e depois melhorar.
    Se for o caso.

--- lib/relex/cli.rb
+++ lib/relex/cli.rb
@@ -1,6 +1,51 @@
+require 'relex/token'
+
 module Relex
   class CLI
     def self.execute(stdout, arguments=[])
+      numero_da_linha = 0
+      comentario = false
+      parenteses = 0
+      tokens = []
+
+      STDIN.read.split('\n').each do |linha|
+        numero_da_linha += 1
+
+        linha.split.each do |token|
+          comentario = true if (token =~ /\{/)
+          comentario = false if (comentario && token =~ /\}/)
+          token.gsub!(/\{[^\}]*\}?/, '')
+          token.gsub!(/.*\}/, '') if comentario
+
+          if token =~ /;|:|,|\(|\)/
+            parenteses += 1 if token =~ /\(/
+            parenteses -= 1 if token =~ /\)/
+            token.gsub!(/;|:|,|\(|\)/, '')
+            tokens << Relex::Token.new(token, :delimitadores)
+          end
+
+          tokens << case token
+              when /[\{\}].*/ then
+                comentario = !comentario
+              when /program|var|begin|end|integer|if|then|else/ then 
+                Relex::Token.new(token, :palavra_reservada)
+              when /\+|\*/
+                Relex::Token.new(token, :operadores_aritmeticos)
+              when /[a..z]+[a..z0..9_]*/
+                Relex::Token.new(token, :identificadores)
+              when /[0..9]+/
+                Relex::Token.new(token, :strings_numericas)
+              when /\:=/
+                Relex::Token.new(token, :comando_atribuicao)
+              when /=/
+                Relex::Token.new(token, :operador_relacional)
+              else
+                "ERRO!"
+            end
+        end
+      end
+
+    puts tokens.inspect
     end
   end
 end
