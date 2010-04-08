3790bcc1e85523242b67f170d2ce8f02ed9e434c
Vítor Baptista <vitor@lavid.ufpb.br>
2010-04-08 07:13:15 -0300

    Adiciona classe Token com regexps para o Pascal

---
+++ lib/relex/token.rb
@@ -0,0 +1,19 @@
+class Token
+  attr_reader :palavras_reservadas, :delimitadores, :operadores_aritmeticos, :identificadores, :strings_numericas, :comando_atribuicao, :operador_relacional
+
+  def initialize(value)
+    self.palavras_reservadas = /program|var|begin|end|integer|if|then|else/
+    self.delimitadores = /;|:|,|\(|\)/
+    self.operadores_aritmeticos = /\+|\*/
+    self.identificadores = /[a..z]+[a..z0..9_]*/
+    self.strings_numericas = /[0..9]+/
+    self.comando_atribuicao = /\:=/
+    self.operador_relacional = /=/
+
+    @value = value
+  end
+
+  def to_s
+    @value
+  end
+end
