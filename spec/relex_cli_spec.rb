4782d91fa918ac0fb14228c37bf91e27c82b94e4
Vítor Baptista <vitor@lavid.ufpb.br>
2010-04-11 20:55:46 -0300

    Adiciona teste de interpretação de comentários

--- spec/relex_cli_spec.rb
+++ spec/relex_cli_spec.rb
@@ -2,14 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'relex/cli'
 
 describe Relex::CLI, "execute" do
-  before(:each) do
-    @stdout_io = StringIO.new
-    Relex::CLI.execute(@stdout_io, [])
-    @stdout_io.rewind
-    @stdout = @stdout_io.read
+  def execute(input)
+    stdout_io = StringIO.new
+    Relex::CLI.execute(stdout_io, input, [])
+    stdout_io.rewind
+    stdout = stdout_io.read
   end
-  
-  it "should print default output" do
-    @stdout.should =~ /To update this executable/
+ 
+  it "should ignore commentaries" do
+    input = "{commentarie}"
+    stdout = execute(input)
+    stdout.should =~ /^$/
   end
 end
