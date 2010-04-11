4e0dccd45255c776d4994c6bef2774fe4c159efb
Vítor Baptista <vitor@lavid.ufpb.br>
2010-04-11 20:32:47 -0300

    Refazendo o algoritmo analisando por char, ao invés de palavra

--- lib/relex/cli.rb
+++ lib/relex/cli.rb
@@ -11,49 +11,13 @@ module Relex
       STDIN.read.split('\n').each do |linha|
         numero_da_linha += 1
 
-        linha.split.each do |token|
-          comentario = true if (token =~ /\{/)
-          comentario = false if (comentario && token =~ /\}/)
-          token.gsub!(/\{[^\}]*\}?/, '')
-          token.gsub!(/.*\}/, '') if comentario
+        linha.split.each_char do |caractere|
+          next if caractere =~ /:blank:/
+          next if comentario
 
-          if token =~ /.*[;:,\(\)].*/
-            case token
-              when /\(/ then
-                parenteses += 1
-                tokens << Relex::Token.new('(', :delimitadores)
-              when /\)/ then
-                parenteses -= 1 if token =~ /\)/
-                tokens << Relex::Token.new(')', :delimitadores)
-              when /;/ then
-                tokens << Relex::Token.new(';', :delimitadores)
-              when /:/ then
-                tokens << Relex::Token.new(':', :delimitadores)
-              when /,/ then
-                tokens << Relex::Token.new(',', :delimitadores)
-            end
-            token.gsub!(/;|:|,|\(|\)/, '')
-          end
-
-          tokens << case token
-              when /[\{\}].*/ then
-                comentario = !comentario
-              when /(program|var|begin|end|integer|if|then|else)/ then 
-                Relex::Token.new(token, :palavra_reservada)
-              when /\+|\*/
-                Relex::Token.new(token, :operadores_aritmeticos)
-              when /[a..z]+[a..z0..9_]*/
-                Relex::Token.new(token, :identificadores)
-              when /[0..9]+/
-                Relex::Token.new(token, :strings_numericas)
-              when /\:=/
-                Relex::Token.new(token, :comando_atribuicao)
-              when /=/
-                Relex::Token.new(token, :operador_relacional)
-              else
-                "ERRO! #{token}"
-            end
-        end
+          comentario = true if token =~ /\{/
+          comentario = false if token =~ /\}/
+       end
       end
 
     puts tokens.inspect
