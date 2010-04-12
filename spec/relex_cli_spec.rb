419ac7822a7c538c21118a531714dc3c6af877c0
Vítor Baptista <vitor@lavid.ufpb.br>
2010-04-12 01:29:27 -0300

    Corrige saídas esperadas e retira redundância de ^$

--- spec/relex_cli_spec.rb
+++ spec/relex_cli_spec.rb
@@ -14,7 +14,7 @@ describe Relex::CLI, "execute" do
     input_tests.each { |input|
       expected_output = formatted_expected_output.gsub('#value#', Regexp.escape(input))
       stdout = run(input)
-      stdout.should =~ Regexp.new(expected_output)
+      stdout.should =~ Regexp.new("^#{expected_output}$")
     }
   end
 
@@ -25,53 +25,53 @@ describe Relex::CLI, "execute" do
 
   it "should print an error message and exit when a char not in the language's alphabet is found" do
     input_tests = ['á', 'é', 'ç', '\"', '@', '#', '@']
-    batch_test(input_tests, "^#value# simbolo_nao_reconhecido \\d+$")
+    batch_test(input_tests, "#value# simbolo_nao_reconhecido \\d+")
   end
 
   it "should detect positive integers" do
     input_tests = ['0', '20', '341', '4123', '59583', '758493', '8098376']
-    batch_test(input_tests, "^#value# numero_inteiro \\d+$")
+    batch_test(input_tests, "#value# numero_inteiro \\d+")
   end
 
   it "should detect non-signed real numbers" do
     input_tests = ['0.3', '20.42', '341.253', '4123.4125', '59583.74585',
                    '758493.254896', '8098376.1245389']
-    batch_test(input_tests, "^#value# numero_real \\d+$")
+    batch_test(input_tests, "#value# numero_real \\d+")
   end
 
   it "should detect reserved words" do
     input_tests = ['program', 'var', 'integer', 'real', 'boolean', 'procedure',
                    'begin', 'end', 'if', 'then', 'else', 'while', 'do']
-    batch_test(input_tests, "^#value# palavra_reservada \\d+$")
+    batch_test(input_tests, "#value# palavra_reservada \\d+")
   end
 
   it "should detect identifiers" do
     input_tests = ['numero', 'a', 'foo5', 'bar32_', 'vida_universo_e_tudo_mais_42']
-    batch_test(input_tests, "^#value# identificador \\d+$")
+    batch_test(input_tests, "#value# identificador \\d+")
   end
 
   it "should detect delimiters" do
     input_tests = [';', '.', ':', '(', ')']
-    batch_test(input_tests, "^#value# identificador \\d+$")
+    batch_test(input_tests, "#value# delimitador \\d+")
   end
 
   it "should detect assignment commands" do
     input_tests = [':=']
-    batch_test(input_tests, "^#value# identificador \\d+$")
+    batch_test(input_tests, "#value# comando_de_atribuicao \\d+")
   end
 
   it "should detect relational operators" do
     input_tests = ['=', '<', '>', '<=', '>=', '<>']
-    batch_test(input_tests, "^#value# identificador \\d+$")
+    batch_test(input_tests, "#value# operador_relacional \\d+")
   end
 
   it "should detect additive operators" do
     input_tests = ['+', '-', 'or']
-    batch_test(input_tests, "^#value# identificador \\d+$")
+    batch_test(input_tests, "#value# operador_aditivo \\d+")
   end
 
   it "should detect multiplicative operators" do
     input_tests = ['*', '/', 'and']
-    batch_test(input_tests, "^#value# identificador \\d+$")
+    batch_test(input_tests, "#value# operador_multiplicativo \\d+")
   end
 end
