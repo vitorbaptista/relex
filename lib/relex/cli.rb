42f4b450688dd2d345dc2e8e5211b2e85ebcc416
Vítor Baptista <vitor@lavid.ufpb.br>
2010-04-11 23:54:15 -0300

    Muitas modificações. Passei um tempo corrigindo o bug e não dei vários commits...
    Preciso criar o hábito de commitar mais frequentemente

--- lib/relex/cli.rb
+++ lib/relex/cli.rb
@@ -9,23 +9,52 @@ module Relex
 
       stdin.read.split('\n').each do |linha|
         tokens_desta_linha = []
+        comentario = false
+        tmp = ''
 
         linha.each_char do |caractere|
-          next if caractere =~ /:blank:/
-          next if comentario
-
           comentario = true if caractere =~ /\{/
           comentario = false if caractere =~ /\}/
+
+          if !tmp.empty? && (caractere =~ /\s/ || comentario)
+            tokens_desta_linha << classifica_token(tmp)
+            tmp = ''
+            next
+          end
+
+          tmp += caractere if caractere =~ /\d/
+        end
+
+        if !tmp.empty? && !comentario
+          tokens_desta_linha << classifica_token(tmp)
+          tmp = ''
         end
 
         tokens << tokens_desta_linha
       end
-
+      
       tokens.each_with_index { |tokens_da_linha, linha|
         tokens_da_linha.each { |token|
-          puts "#{token} #{linha + 1}"
+          stdout.puts "#{token} #{linha + 1}"
         }
       }
+    end
+
+    def self.classifica_token(token)
+      case token
+        when /(program|var|begin|end|integer|if|then|else)/ then 
+          Relex::Token.new(token, :palavra_reservada)
+        when /\+|\*/
+          Relex::Token.new(token, :operador_aritmeticos)
+        when /[a-z]+[a-z0-9_]*/
+          Relex::Token.new(token, :identificador)
+        when /[0-9]+/
+          Relex::Token.new(token, :numero_inteiro)
+        when /\:=/
+          Relex::Token.new(token, :comando_de_atribuicao)
+        when /=/
+          Relex::Token.new(token, :operador_relacional)
+      end
     end
   end
 end
